2a487b46b16ccfb0475150d377599f058ecb0bb5
Pablo Herrero Ferrán <120820455+pabloherrerof@users.noreply.github.com>
2023-11-10 13:00:41 +0100

    name change

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MiViajeP1
+# MiViajeP2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MiViajeP1'`, () => {
+  it(`should have as title 'MiViajeP2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MiViajeP1');
+    expect(app.title).toEqual('MiViajeP2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MiViajeP1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MiViajeP2 app is running!');
   });
 });
